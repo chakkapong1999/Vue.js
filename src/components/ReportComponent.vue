7a8b21d28fd24db8313945598ae3a839984295eb
Chakkapong <chakkapong1999@gmail.com>
2021-12-22 22:35:09 +0700

    more css

--- src/components/ReportComponent.vue
+++ src/components/ReportComponent.vue
@@ -1,42 +1,54 @@
 <template>
   <div class="container">
-    <div class="grid-cell1">
+    <div class="grid-item1">
       <h3>ติดเชื้อเพิ่มขึ้น</h3>
       <h1>+ {{ formatNumber(info.new_case) }}</h1>
-      <p>
-        สะสม <b> {{ formatNumber(info.total_case) }}</b>
-      </p>
+      <div class="total1">
+        <p>
+          สะสม <b> {{ formatNumber(info.total_case) }}</b>
+        </p>
+      </div>
     </div>
-    <div class="grid-cell2">
+    <div class="grid-item2">
       <h3>เสียชีวิตเพิ่มขึ้น</h3>
       <h1>+ {{ formatNumber(info.new_death) }}</h1>
-      <p>
-        สะสม <b> {{ formatNumber(info.total_death) }}</b>
-      </p>
+      <div class="total2">
+        <p>
+          สะสม <b> {{ formatNumber(info.total_death) }}</b>
+        </p>
+      </div>
     </div>
-    <div class="grid-cell3">
+    <div class="grid-item3">
       <h3>รักษาตัวในโรงพยาบาลลดลง</h3>
       <h1>- 2525</h1>
-      <p>สะสม <b>2415613543 </b></p>
+      <div class="total3">
+        <p>สะสม <b>2415613543 </b></p>
+      </div>
     </div>
-    <div class="grid-cell4">
+    <div class="grid-item4">
       <h3>หายแล้วเพิ่มขึ้น</h3>
       <h1>+ {{ formatNumber(info.new_recovered) }}</h1>
-      <p>
-        สะสม <b>{{ formatNumber(info.total_recovered) }} </b>
-      </p>
+      <div class="total4">
+        <p>
+          สะสม <b>{{ formatNumber(info.total_recovered) }} </b>
+        </p>
+      </div>
     </div>
-    <div class="grid-cell5">
+    <div class="grid-item5">
       <h3>ผู้รับวัคซีน โดส 1</h3>
       <h1>+ 2525</h1>
-      <p>สะสม <b>2415613543</b></p>
+      <div class="total5-6">
+        <p>สะสม <b>2415613543</b></p>
+      </div>
     </div>
-    <div class="grid-cell6">
+    <div class="grid-item6">
       <h3>ผู้รับวัคซีน โดส 2</h3>
       <h1>+ 2525</h1>
-      <p>สะสม <b>2415613543 </b></p>
+      <div class="total5-6">
+        <p>สะสม <b>2415613543 </b></p>
+      </div>
     </div>
-    <div class="grid-cell7">
+    <div class="grid-item7">
       <h3>สะสม 76.37% ของประชากร</h3>
       <p>
         นับเฉพาะเข็มแรกฉีดแล้ว 50546545 คน ไม่นับรวมโดสที่สองที่ฉีดซ้ำให้คนเดิม
@@ -80,47 +92,68 @@ b {
   grid-template-rows: auto auto;
   align-content: center;
   color: white;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
-.grid-cell1 {
+.grid-item1 {
   grid-column: 1/3;
   grid-row: 1/3;
   border-radius: 20px 0px 0px 0px;
   text-indent: 20px;
-  background: rgba(255, 0, 0, 0.5);
+  background: rgba(188, 52, 64, 0.7);
 }
-.grid-cell2 {
+.grid-item2 {
   grid-column: 3/5;
   grid-row: 1/3;
   border-radius: 0px 20px 0px 0px;
   text-indent: 20px;
-  background: rgba(64, 51, 71, 0.5);
+  background: rgba(100, 94, 110, 0.7);
 }
 
-.grid-cell3 {
+.grid-item3 {
   grid-column: 1/3;
   text-indent: 20px;
-  background: rgba(80, 28, 80, 0.5);
+  background: rgba(122, 165, 179, 0.7);
 }
-.grid-cell4 {
+.grid-item4 {
   grid-column: 3/5;
   text-indent: 20px;
-  background: rgba(74, 148, 45, 0.9);
+  background: rgba(99, 177, 110, 0.8);
 }
-.grid-cell5 {
+.grid-item5 {
   grid-column: 1/3;
   text-indent: 20px;
-  background: rgba(83, 136, 63, 0.8);
+  background: rgba(191, 170, 104, 0.8);
 }
-.grid-cell6 {
+.grid-item6 {
   grid-column: 3/5;
   text-indent: 20px;
-  background: rgb(83, 136, 63, 0.8);
+  background: rgba(191, 170, 104, 0.8);
 }
-.grid-cell7 {
+.grid-item7 {
   grid-column: 1/5;
   border-radius: 0px 0px 30px 30px;
   text-align: center;
-  background: rgba(40, 56, 33, 0.5);
+  background: rgba(100, 97, 78, 0.9);
+}
+
+.total1 {
+  overflow: auto;
+  background: rgba(148, 73, 75, 0.8);
+}
+.total2 {
+  overflow: auto;
+  background: rgba(86, 82, 91, 0.8);
+}
+.total3 {
+  overflow: auto;
+  background: rgba(100, 125, 132, 0.8);
+}
+.total4 {
+  overflow: auto;
+  background: rgba(87, 133, 91, 0.8);
+}
+.total5-6 {
+  overflow: auto;
+  background: rgba(135, 132, 94, 0.8);
 }
 </style>
